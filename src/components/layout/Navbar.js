24ddfe1955dde7f9c2f2b52f16d420f05228fe53
abhisheksingh98 <writetoabhishek07@gmail.com>
2019-02-18 19:21:20 +0530

    Initial Commit

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-dark bg-dark mb-5">
+      <span className="navbar-brand  h1 mx-auto mb-0">LyricFinder</span>
+    </nav>
+  );
+};
+
+export default Navbar;
